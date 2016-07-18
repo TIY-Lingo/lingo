d254e76df2619add9cc12f69aed22cc285c51475
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-07-18 14:32:14 -0400

    Initial Import

---
+++ src/main/java/com/theironyard/LingoApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingoApplication.class, args);
+	}
+}
