c1edb6b90bb3022ef9ec6401aaf73e55861b2b79
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-20 15:46:17 -0400

    click event for sign up

--- js/controllers/UserController.js
+++ js/controllers/UserController.js
@@ -14,6 +14,17 @@ module.exports = function(app) {
                 }
             }
 
+            $scope.signUp = function() {
+                console.log("clicked log in");
+                UserService.postUserInfo($scope.username)
+                console.log('clicked')
+                if ($scope.username != null) {
+                    $location.path('/home');
+                } else {
+                    alert('Please enter a username');
+                }
+            }
+
         }
     ]);
 }
