ece7ba441cfa1b7a100f3aea3da2e92b9581c508
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-20 14:06:30 -0400

    added post request for sign in and get request for articles

--- js/services/UserService.js
+++ js/services/UserService.js
@@ -1,27 +1,20 @@
 module.exports = function(app) {
 
     app.factory('UserService', ['$http', function($http) {
-        let loginName = "";
 
-        createLogin: function(name, password) {
-          loginName = name;
-          console.log(loginName);
-
-                $http({
-                    method: 'POST',
-                    url: '/login',
-                    data: {
-                        username: password:
-                    }
-                }).then(function(response) {
-                    let userInfoObject = response.data;
-                    console.log("object with user information", userInfoObject);
-                });
-            },
-            return {
-                sendLoginInfo: function() {
-                    return loginName;
-                }
-            }
+    ////signIn() click event to post username and password to server//////
+      return {
+      postUserInfo: function(name) {
+          $http({
+              url: '/login',
+              method: 'POST',
+              data: {
+                  username: name,
+                  password: "1234",
+              },
+          }).then(function(results) {
+              console.log("posted")
+          });
+      },
     }]);
 };
