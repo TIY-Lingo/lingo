c5675a718836ee028f44b6eea41cd395ea084d9e
Amanda Schuff <amandaschuff@Amandas-MacBook-Pro.local>
2016-07-20 14:06:25 -0400

    user preferences post request

--- js/services/UserService.js
+++ js/services/UserService.js
@@ -11,16 +11,40 @@ module.exports = function(app) {
                     method: 'POST',
                     url: '/login',
                     data: {
-                        username: password:
+                        username: ,
+                        password: ,
                     }
                 }).then(function(response) {
                     let userInfoObject = response.data;
                     console.log("object with user information", userInfoObject);
                 });
             },
+        createPreferences: function(pref)  {
+          userPref = pref;
+          console.log(userPref);
+
+          $http({
+              method: 'POST',
+              url: '/preferences',
+              data: {
+                language: 'spanish',
+                business: false,
+                sports: false,
+                politics: false,
+                technology: false,
+                arts: false,
+              }
+          }).then(function(response) {
+              let userPrefObject = response.data;
+              console.log("object with user preferences", userPrefObject);
+          });
+        } ,
             return {
                 sendLoginInfo: function() {
                     return loginName;
+                },
+                sendPrefInfo: function() {
+                  return userPref;
                 }
             }
     }]);
