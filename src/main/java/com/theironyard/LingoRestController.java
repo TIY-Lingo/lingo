a94e86c1e24b6ee29a48e0697200d0fd122f6fb8
John Crooks <johnmcrooks@gmail.com>
2016-07-21 17:07:52 -0400

    Can't remember if I've pushed lately

--- src/main/java/com/theironyard/LingoRestController.java
+++ src/main/java/com/theironyard/LingoRestController.java
@@ -193,13 +193,15 @@ public class LingoRestController {
                         count++;
                     } else if (count == 20) {
                         article.setSpan1(spanishArticle);  //once the count hits 6 save the spanish changes and save it back to the DB
-                        articles.save(article);
+                        articles.save(article);            //add the article to the DB in case of failure further down the chain
+                        levelTwoReplacement(article, spanishArticle);   //Take the article and the translated String and pass it down a the chain
                         break;
                     } else{
                         failedcount++;
-                        if (failedcount >4000){
+                        if (failedcount >5000){
                             article.setSpan1(spanishArticle);
                             articles.save(article);
+                            levelTwoReplacement(article, spanishArticle);
                             System.out.println("THIS ARTICLE DOESN'T HAVE ENOUGH WORDS TO TRANSLATE");
                             break;
                         }
@@ -208,6 +210,56 @@ public class LingoRestController {
             }
         }
         System.out.println("Finished Processing Articles...");
+    }
+
+    public void levelTwoReplacement(Article article, String level1){
+        int count2=0;
+        int failedcount=0;
+        while (count2 <= 20) {
+            int seedValue = randomNum();   //grab a random number to check for in the article
+            if (level1.contains(dictionaries.findOne(seedValue).getEnglish())) {   //if the article contains the randomly selected english word from the language dictionary....
+                level1 = level1.replace(dictionaries.findOne(seedValue).getEnglish(), "<span class='spanish'>" + dictionaries.findOne(seedValue).getSpanish() +"</span>"); //replace english seed value with spanish seed value
+                count2++;
+            } else if (count2 == 20) {
+                article.setSpan2(level1);  //once the count hits 6 save the spanish changes and save it back to the DB
+                articles.save(article);
+                levelThreeReplacement(article, level1);
+                break;
+            } else{
+                failedcount++;
+                if (failedcount >5000){
+                    article.setSpan2(level1);
+                    articles.save(article);
+                    levelThreeReplacement(article, level1);
+                    System.out.println("Level 2 THIS ARTICLE DOESN'T HAVE ENOUGH WORDS TO TRANSLATE");
+                    break;
+                }
+            }
+        }
+    }
+
+    public void levelThreeReplacement(Article article, String level2){
+        int count2=0;
+        int failedcount=0;
+        while (count2 <= 20) {
+            int seedValue = randomNum();   //grab a random number to check for in the article
+            if (level2.contains(dictionaries.findOne(seedValue).getEnglish())) {   //if the article contains the randomly selected english word from the language dictionary....
+                level2 = level2.replace(dictionaries.findOne(seedValue).getEnglish(), "<span class='spanish'>" + dictionaries.findOne(seedValue).getSpanish() +"</span>"); //replace english seed value with spanish seed value
+                count2++;
+            } else if (count2 == 20) {
+                article.setSpan2(level2);  //once the count hits 6 save the spanish changes and save it back to the DB
+                articles.save(article);
+                break;
+            } else{
+                failedcount++;
+                if (failedcount >5000){
+                    article.setSpan3(level2);
+                    articles.save(article);
+                    System.out.println("Level 3 THIS ARTICLE DOESN'T HAVE ENOUGH WORDS TO TRANSLATE");
+                    break;
+                }
+            }
+        }
     }
 
     public void parseDictionary() throws FileNotFoundException {
