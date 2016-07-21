92b6c60d9c5a20a66ae5a93b2bbeda4be5c4faea
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-21 12:25:30 -0400

    working on validation for registration

--- js/controllers/UserController.js
+++ js/controllers/UserController.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
             $scope.signUp = function() {
                 console.log("clicked sign up");
                 UserService.postUserInfo($scope.userInput, $scope.userPassword)
-                $location.path('/preferences');
+                // $location.path('/preferences');
                 // if ($scope.username != null) {
                 //     $location.path('/home');
                 // } else {
