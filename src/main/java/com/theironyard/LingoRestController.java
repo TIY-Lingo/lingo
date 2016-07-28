ba3b5abfe371d0ab9a5512b9d0731a4b6e141d7f
John Crooks <johnmcrooks@gmail.com>
2016-07-28 12:11:07 -0400

    Merged DontLookAtMe with Lingo

--- src/main/java/com/theironyard/LingoRestController.java
+++ src/main/java/com/theironyard/LingoRestController.java
@@ -1,12 +1,14 @@
 package com.theironyard;
 
-import com.theironyard.entities.Article;
-import com.theironyard.entities.Dictionary;
-import com.theironyard.entities.User;
+import com.theironyard.entities.*;
+import com.theironyard.services.CategoryRepository;
 import com.theironyard.services.DictionaryRepository;
 import com.theironyard.utils.PasswordStorage;
 import org.h2.tools.Server;
 
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,15 +19,16 @@ import com.theironyard.services.ArticleRepository;
 import com.theironyard.services.UserRepository;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.sql.SQLException;
 
 
 @RestController
@@ -40,11 +43,33 @@ public class LingoRestController {
     @Autowired
     DictionaryRepository dictionaries;
 
+    @Autowired
+    CategoryRepository categories;
+
+    Server dbui = null;
+
     @PostConstruct
     public void init() throws SQLException, IOException, InterruptedException {
-        Server.createWebServer().start();
+        dbui.createWebServer().start();
         parseDictionary();
+
+        Category cat = new Category("business", 1);
+        System.out.println(cat);
+        if (categories.count() == 0) {
+            categories.save(cat);
+            cat = new Category("politics",2);
+            categories.save(cat);
+            cat = new Category("sports",3);
+            categories.save(cat);
+            cat = new Category("arts",4);
+            categories.save(cat);
+            cat = new Category("technology",5);
+            categories.save(cat);
+        }
     }
+
+    @PreDestroy
+    public void destroy(){dbui.stop();}
 
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
@@ -58,11 +83,9 @@ public class LingoRestController {
             return false;
         }
 //        user1.setTimestamp(LocalDateTime.now());
-        users.save(user1);
+//        users.save(user1);
         session.setAttribute("username", user1.getUsername());
         return true;
-
-
     }
 
     @RequestMapping(path = "/registerUser", method = RequestMethod.POST)
@@ -71,6 +94,7 @@ public class LingoRestController {
             return false;
         }else {                                                 //Otherwise create the user and add it to the DB
             User user1 = new User(user.getUsername(), PasswordStorage.createHash(user.getPassword()));
+
             users.save(user1);
             session.setAttribute("username", user1.getUsername());
             return true;
@@ -91,45 +115,65 @@ public class LingoRestController {
         if (session.getAttribute("username") ==null){
             throw new Exception("You must Login to view or change preferences!");
         }else {
-
             User userA = users.findByUsername((String) session.getAttribute("username"));
-            userA.setArts(user.getArts());
-            userA.setBusiness(user.getBusiness());
             userA.setLanguage(user.getLanguage());
-            userA.setPolitics(user.getPolitics());
-            userA.setSports(user.getSports());
-            userA.setTechnology(user.getTechnology());
+            if (user.getArts()) {
+                Category cat = categories.findFirstByType("arts");
+                userA.getCatList().add(cat);
+            }
+            if(user.getBusiness()){
+                Category cat = categories.findFirstByType("business");
+                userA.getCatList().add(cat);
+            }
+            if(user.getPolitics()){
+                Category cat = categories.findFirstByType("politics");
+                userA.getCatList().add(cat);
+            }
+            if(user.getSports()){
+                Category cat = categories.findFirstByType("sports");
+                userA.getCatList().add(cat);
+            }
+            if(user.getTechnology()){
+                Category cat = categories.findFirstByType("technology");
+                userA.getCatList().add(cat);
+            }
+
             users.save(userA);
             System.out.println("User saved to Database...");
         }
     }
 
     @RequestMapping(path = "/articles", method = RequestMethod.GET)
-    public Iterable<Article> getArticles(HttpSession session) throws Exception {
+    public ArrayList<ReturnArticle> getArticles(HttpSession session) throws Exception {
         if (session.getAttribute("username")==null){
             throw new Exception("You must log in to view this page");
         }else {
             User user = users.findByUsername((String) session.getAttribute("username"));
 
-//            ArrayList<Iterable<Article>> articleList = new ArrayList<>();
-//            if (user.getTechnology()){
-//                articleList.add((Iterable<Article>) articles.findArticleByType("technology"));
-//            }
-//            if(user.getSports()){
-//                articleList.add((Iterable<Article>) articles.findArticleByType("sports"));
-//            }
-//            if(user.getPolitics()){
-//                articleList.add((Iterable<Article>) articles.findArticleByType("politics"));
-//            }
-//            if(user.getArts()){
-//                articleList.add((Iterable<Article>) articles.findArticleByType("arts"));
-//            }
-//            if (user.getBusiness()){
-//                articleList.add((Iterable<Article>) articles.findArticleByType("business"));
-//            }
-        return articles.findAll();
-        }
+            String sql = "SELECT ca.CATEGORY_ID, " + "a.span1 " + ", a.title, a.type"  + "  FROM Articles a " +
+            "INNER JOIN CATEGORY_ARTICLE ca ON ca.article_id = a.ID " +
+            "INNER JOIN USERS_CATEGORIES uc on uc.catlist_ID = ca.CATEGORY_ID " +
+            "WHERE uc.user_id = ? ";
 
+            Connection conn = DriverManager.getConnection("jdbc:h2:./main;DB_CLOSE_ON_EXIT=FALSE", "sa", "");
+            PreparedStatement stmnt = conn.prepareStatement(sql);
+            stmnt.setInt(1, user.getId());
+            ResultSet results = stmnt.executeQuery();
+
+            ArrayList<ReturnArticle> returnArray = new ArrayList<>();
+            while(results.next()){
+                int categoryId = results.getInt(1);
+                String translation = results.getString(2);
+                String title = results.getString(3);
+                String type = results.getString(4);
+                ReturnArticle ra1 = new ReturnArticle(categoryId, translation, title, type);
+                returnArray.add(ra1);
+            }
+
+            conn.close();
+
+            return returnArray;
+        }
     }
 
     @RequestMapping(path = "/logout", method = RequestMethod.POST)
